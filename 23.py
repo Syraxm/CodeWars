cb227d0db9aa67748e3bef3f76be36652aa8b64f
Syraxm <tyomachannel@gmail.com>
2022-11-29 18:58:09 +0300

    hoooo

---
+++ 23.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
+def high_and_low(numbers):
+
+    # Разделяем наш аргумент на части (перевод строки в массив)
+    numbers1 = numbers.split()
+    # Новый лист для повторения списка numbers1, но в int
+    new_list = []
+    # Цикл для добавления всех чисел, но в int
+    for i in numbers1:
+        new_list.append(int(i))
+    # Возвращаем строку с макс. и мин. значением
+    # *Не забудем про пробел*
+    return str(max(new_list)) + " " + str(min(new_list))
+
+    # Объяснять в принципе не было смыла, но ладно
